54398fecbb2693c8130cd186c8ce18d938663066
muhammadisa226 <muhammadisa226@gmail.com>
2023-10-26 18:18:27 +0700

    delete all files

--- src/application/web.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import { publicRouter } from "../routes/publicApi.js";
-import { errorMiddleware } from "../middleware/errorMiddleware.js";
-export const web = express();
-web.use(cors());
-web.use(express.json());
-web.use(publicRouter);
-web.use(errorMiddleware);
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import { web } from "./application/web.js";
-import { logger } from "./application/logging.js";
-web.listen(process.env.WEB_PORT, process.env.WEB_HOST, () => {
-  logger.info(
-    `App Start in http://${process.env.WEB_HOST}:${process.env.WEB_PORT}`
-  );
-});
