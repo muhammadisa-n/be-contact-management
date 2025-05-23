2169cba5e34830e72b25533d4b3916ccdc275e17
muhammadisa-n <muhammadisa226@gmail.com>
2025-05-23 13:10:37 +0700

    fix

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userControiller from "../controllers/user-controiller.js";
 const publicRouter = new express.Router();
-publicRouter.post("/api/users", userControiller.register);
-publicRouter.post("/api/users/login", userControiller.login);
+publicRouter.post("/api/auth/register", userControiller.register);
+publicRouter.post("/api/auth/login", userControiller.login);
 export { publicRouter };
