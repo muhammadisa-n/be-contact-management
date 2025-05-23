145e10160a26cf23a6703bc6c613e8d0cb6e332d
muhammadisa-n <muhammadisa226@gmail.com>
2025-05-23 09:25:30 +0700

    add ci cd config

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -2,5 +2,5 @@ import express from "express";
 import userControiller from "../controllers/user-controiller.js";
 const publicRouter = new express.Router();
 publicRouter.post("/api/users/register", userControiller.register);
-publicRouter.get("/api/users/login", userControiller.login);
+publicRouter.post("/api/users/login", userControiller.login);
 export { publicRouter };
