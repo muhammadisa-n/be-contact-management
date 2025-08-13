f19c38e543927e243e45e287a2815a3f2004b074
muhammadisa-n <muhammadisa226@gmail.com>
2025-08-13 14:39:41 +0700

    add cors config

--- src/application/web.js
+++ src/application/web.js
@@ -3,9 +3,16 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { mainRouter } from "../routes/main-router.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import "dotenv/config";
+dotenv.config();
 export const web = express();
 web.use(express.json());
-web.use(cors());
+web.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 web.use(cookieParser());
 web.use(mainRouter);
 web.use(errorMiddleware);
