0f310a63fa1b5b4cc591959f543b005a507949ba
muhammadisa-n <muhammadisa226@gmail.com>
2025-08-13 15:05:47 +0700

    add axios

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,7 +1,7 @@
 import express from "express";
 import userControiller from "../controllers/user-controiller.js";
 import "dotenv/config";
-
+import axios from "axios";
 const publicRouter = new express.Router();
 publicRouter.post("/api/users/register", userControiller.register);
 publicRouter.post("/api/users/login", userControiller.login);
@@ -12,7 +12,7 @@ publicRouter.get("/api/verify-sso", async (req, res) => {
   try {
     const token = req.cookies.sso_token;
     if (!token) return res.status(401).json({ error: "Not logged in" });
-
+    console.log(token);
     const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/me`, {
       headers: { Authorization: `Bearer ${token}` },
     });
